e30a2a90b069b0751faf82483ead6e6025de776a
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-11-08 17:24:52 +0000

    faet: updates

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 import express from "express";
 import axios from "axios";
 import "dotenv/config";
-import morgan from "morgan";
-
-if (!process.env.API_KEY) {
-  console.error("Missing API_KEY in environment variables");
-  process.exit(1);
-}
 
 const app = express();
 const port = 3000;
-
-const cors = require("cors");
-app.use(cors());
-
-app.use(morgan("combined"));
 
 app.get("/", async (req, res) => {
   try {
@@ -26,14 +15,9 @@ app.get("/", async (req, res) => {
         },
       }
     );
-    res.type("application/json");
     res.json(response.data);
   } catch (error) {
-    console.error(error);
-    res.type("application/json");
-    res
-      .status(500)
-      .json({ error: "An error occurred while processing your request." });
+    res.json({ error: error.toString() });
   }
 });
 
