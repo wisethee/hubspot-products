02c7110d416a781304305a673fc8ded790209c4e
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-11-09 09:15:09 +0000

    updates

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 import express from "express";
 import axios from "axios";
-import cors from "cors";
 
 import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
+if (!process.env.API_KEY) {
+  console.error("Missing API_KEY in environment variables");
+  process.exit(1);
+}
+
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(
-      "https://api.hubapi.com/crm/v3/objects/products",
+      "https://api.hubapi.com/crm/v3/objects/products?limit=10&properties=hs_sku&archived=false",
       {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
         },
       }
     );
+    res.type("application/json");
+    res.json(response.data);
     res.json(response.data);
   } catch (error) {
     res.json({ error: error.toString() });
