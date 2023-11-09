91ed12f46cbe98297e09c2ff539aabc6a6388b2b
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-11-09 09:50:25 +0000

    updates

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// import express from "express";
+// import axios from "axios";
+// import cors from "cors";
+
+// import "dotenv/config";
+
+// const app = express();
+// const port = 3000;
+// const axiosUrl =
+//   "https://api.hubapi.com/crm/v3/objects/products?limit=10&properties=hs_sku%2C%20hs_folder_name%2C%20name%2C%20description%2C%20price&archived=false";
+
+// app.use(cors());
+
+// if (!process.env.API_KEY) {
+//   console.error("Missing API_KEY in environment variables");
+//   process.exit(1);
+// }
+
+// app.get("/", async (req, res) => {
+//   try {
+//     const response = await axios.get(axiosUrl, {
+//       headers: {
+//         Authorization: `Bearer ${process.env.API_KEY}`,
+//       },
+//     });
+//     res.type("application/json");
+//     res.json(response.data);
+//   } catch (error) {
+//     res.json({ error: error.toString() });
+//   }
+// });
+
+// app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
 import express from "express";
 import axios from "axios";
 import cors from "cors";
@@ -6,6 +40,8 @@ import "dotenv/config";
 
 const app = express();
 const port = 3000;
+const axiosUrl =
+  "https://api.hubapi.com/crm/v3/objects/products?limit=10&properties=hs_sku%2C%20hs_folder_name%2C%20name%2C%20description%2C%20price&archived=false";
 
 app.use(cors());
 
@@ -16,16 +52,31 @@ if (!process.env.API_KEY) {
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get(
-      "https://api.hubapi.com/crm/v3/objects/products?limit=10&properties=hs_sku%2C%20hs_folder_name%2C%20name%2C%20description%2C%20price&archived=false",
-      {
+    let allResults = [];
+
+    let url = axiosUrl;
+
+    while (url) {
+      const response = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
         },
+      });
+
+      const { results, paging } = response.data;
+      allResults = allResults.concat(results);
+
+      // Check if there is a next page
+      if (paging && paging.next && paging.next.link) {
+        url = paging.next.link;
+      } else {
+        // No more pages, break the loop
+        url = null;
       }
-    );
+    }
+
     res.type("application/json");
-    res.json(response.data);
+    res.json({ results: allResults });
   } catch (error) {
     res.json({ error: error.toString() });
   }
